eed543ad19c37986c8395b793fa9ee0ee3fc65ac
sudipt1999 <sudiptdabral2991999@gmail.com>
2019-10-17 00:51:02 +0530

    project finished

---
+++ main.js
@@ -0,0 +1,43 @@
+
+let search_btn = document.getElementById('btn')
+let search_input = document.getElementById('pokemon_input')
+let dom_pokemon = document.getElementById('pokemon-search-result')
+
+function onSearchButtonPressed (event) {
+   let input = search_input.value
+   if(input.length == 0)
+    return
+
+    $.get(`https://pokeapi.co/api/v2/pokemon/${input}`,  // url
+      function (data, textStatus, jqXHR) {  // success callback
+         // alert('status: ' + textStatus + ', data:' + data);
+         // console.log(data)
+          dom_pokemon.innerHTML = `
+          <div class="col-12 col-md-4 col-md-3 m-auto pokemon-card">
+          <img src="${data.sprites.back_default}" alt="pokemon" class="pokemon-image">
+          <div class="pokemon-card-name">
+              <span class="name">${data.name}</span>
+          </div>
+          <div class="pokemon-card-stats py-3">
+              <h4 class="stats-title">Stats</h4>
+              <strong class="stats d-block">Weight: ${data.weight}</strong>
+              <strong class="stats d-block">Base Experience: ${data.base_experience}</strong>
+              <strong class="stats d-block">Height: ${data.height}</strong>
+              <strong class="stats d-block">Order: ${data.order}</strong>
+          </div>
+         </div>
+          
+          `
+    }).fail(function(){
+        dom_pokemon.innerHTML=`<h3 class="error-text">Sorry No Result Found ! </h3>`
+    });
+
+
+
+}
+
+search_btn.addEventListener("click", function(event){
+    onSearchButtonPressed(event)
+})
+
+
